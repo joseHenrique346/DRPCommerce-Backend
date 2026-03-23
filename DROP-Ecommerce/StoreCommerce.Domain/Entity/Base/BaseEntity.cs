fc12a8edb2102c63d55a912fa17e512b54f95fd0
JuliaCapellini <capellini.julia2005@gmail.com>
2026-03-23 20:07:59 -0300

    feat(Entity): Criada base de entidade

---
+++ DROP-Ecommerce/StoreCommerce.Domain/Entity/Base/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace StoreCommerce.Domain.Entity;
+
+public class BaseEntity
+{
+    #region Properties
+    public long Id { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? UpdatedAt { get; private set; }
+    #endregion
+
+    #region Functions
+    public void SetChangedDate()
+    {
+        UpdatedAt = DateTime.UtcNow;
+    }
+    #endregion
+}
