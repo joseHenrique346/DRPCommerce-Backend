0a567f86d8b0d413830fde1f32754728eae4d676
CarlosLeutcke <Ch772707@gmail.com>
2026-04-23 20:18:58 -0300

    feat(Result): Criado Objeto de Result

---
+++ DROP-Ecommerce/StoreCommerce.Application/Result/Result.cs
@@ -0,0 +1,32 @@
+﻿namespace StoreCommerce.Application.Result
+{
+    internal class Result<TContent>
+    {
+        public bool IsSuccess { get; private set; } = true;
+        public TContent Content { get; private set; }
+        public List<string> ListMessageErrors { get; private set; }
+        
+        public void Failure(string message)
+        {
+            IsSuccess = false;
+            ListMessageErrors.Add(message);
+        }
+
+        public static Result<TContent> Success(TContent content)
+        {
+            return new Result<TContent>
+            {
+                Content = content,
+            };
+        }
+
+        public static Result<TContent> FailureFromList(List<string> errors)
+        {
+            var result = new Result<TContent> { };
+            result.IsSuccess = false;
+            result.ListMessageErrors.AddRange(errors);
+            return result;
+
+        }
+    }
+}
