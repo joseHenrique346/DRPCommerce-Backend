41a769482dca1e462dbc593c707be5c3e33f77c2
José Henrique <josehenriquefernandes346@gmail.com>
2026-05-26 14:47:47 -0300

    feat(Command): Criado CommandValidators no DROP.

--- DROP-Ecommerce/DropCommerce.Application/Features/Commands/QueueEntry/Delete/DeleteQueueEntryCommandValidator.cs
+++ DROP-Ecommerce/DropCommerce.Application/Features/Commands/QueueEntry/Delete/DeleteQueueEntryCommandValidator.cs
@@ -1,5 +1,11 @@
+using FluentValidation;
+
 namespace DropCommerce.Application.Features.Commands;
 
-public class DeleteQueueEntryCommandValidator
+public class DeleteQueueEntryCommandValidator : AbstractValidator<DeleteQueueEntryCommand>
 {
+    public DeleteQueueEntryCommandValidator()
+    {
+        RuleFor(x => x.id).GreaterThan(0);
+    }
 }
